a66392627c0d6a2d54d730310edd8c67ef755269
llindemann <leonie.lindemann.ll@gmail.com>
2024-07-04 19:49:19 +0200

    feat; move_and_collide of player

--- Player.gd
+++ Player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 10.0
+const JUMP_VELOCITY = -4000.0
 @onready var _animatedSprite = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -13,14 +13,10 @@ func _ready():
 	
 func _physics_process(delta):
 	process_animation()
-	# Add the gravity.
+	#Add the gravity.
 	#if not is_on_floor():
 		#velocity.y += gravity * delta
-
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_vector("ui_left", "ui_right",  "ui_up", "ui_down")
@@ -30,7 +26,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 
-	move_and_slide()
+	move_and_collide(velocity)
 
 func setup_animation():
 	_animatedSprite.animation = "idle"
